fcbc43618bf9011b9a1326c9cc6de5cbb4e78971
Youssefsaoud07 <76500497+Youssefsaoud07@users.noreply.github.com>
2021-09-03 17:30:37 +0200

    Update constants.js

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -4,7 +4,8 @@ export const projects = [
     description: "Using React, Node.js, Express & MongoDB you'll learn how to build a Full Stack MERN Application - from start to finish. The App is called Memories and it is a simple social media app that allows users to post interesting events that happened in their lives.",
       image: '/images/image1.jpeg',
       tags: ['FireStore', 'Tailwind', 'React Native'],
-    source: 'https://github.com/Youssefsaoud07/Binetna-react-native_graphql_app-',
+    source: 'https://github.com/Youssefsaoud07/perfect_master_app',
+    
     visit: 'https://www.upwork.com/freelancers/~018af2428bae4bdad7?viewMode=1',
     id: 0,
   },
@@ -13,7 +14,7 @@ export const projects = [
     description:"While building it you're going to learn many advanced React & JavaScript topics, as well as how to use Stripe for card transactions. On top of that, at the end of the video, you will have this unique and complex webshop app that you will be able to add to your portfolio. And trust me, e-commerce applications are impressive.",
     image: '/images/img2.jpeg',
     tags: ['React', 'JavaScript'],
-    source: 'https://github.com/Youssefsaoud07/perfect_master_app',
+    source: 'https://github.com/Youssefsaoud07/Binetna-react-native_graphql_app-',
     visit: 'https://www.upwork.com/freelancers/~018af2428bae4bdad7?viewMode=1',
     id: 1,
   },
